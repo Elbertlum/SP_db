80c191a4f68fc684d471d229cb644bd53389c5d7
elbert <elbert.lum@gmail.com>
2017-02-04 21:53:15 -0800

    Added dependencies, .env file, connection to db

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(process.env.DATABASE_URL);
+
+sequelize
+  .authenticate()
+  .then(function() {
+    console.log('Connection to database established successfully');
+  })
+  .catch(function(err) {
+    console.log('Unable to connect to database: ', err);
+  });
 
 app.use('/', express.static('client'));
+
+app.post('/country');
+
+app.post('/adminarea');
+
+app.post('/city');
 
 app.listen(3000, function () {
   console.log('listening on port 3000!');
